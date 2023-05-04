df45819eb597e8eda9c3db87c716b1cb416852f8
Alex Cipollone <alex.cipollone10@gmail.com>
2023-05-04 12:41:20 -0400

    more updates

--- TODO.md
+++ TODO.md
@@ -6,15 +6,16 @@ Project Description
 
 ### Todo
 
-- [ ] Center and reduce card widths  
-- [ ] Create project videos  
-- [ ] Set and switch active link
+- [ ] Create project video   
+- [ ] Make cards look nice
+- [ ] Change bg-primary to Kellenberg blue background using SASS
+- [ ] SASS media queries - keep making mobile-friendly (i.e. navbar dropdown)
 
 ### In Progress
 
 ### Done ✓
 
-- [x] Make cool logos for bullet poitns  
+- [x] Make cool logos for bullet points
 - [x] Bring in my nice picture  
 - [x] Properly center and position navbar  
 - [x] Make navbar persistent  
@@ -23,3 +24,8 @@ Project Description
 - [x] Make a commit to GitHub  
 - [x] Write an About Me  
 - [x] Insert icons & links to social media  
+- [x] Set and switch active link
+- [x] Center and reduce card widths  
+- [x] Fixed active link issue where, if my name was clicked, another active link would still be designated
+- [x] Create footer  
+- [x] Fix "My Cool Drum Machine" text  
--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-let activeLink = document.getElementById('about-link');
-console.log(activeLink.innerText);
+let activeLink;
 
 function changeActiveLink(){
-    if (activeLink.innerText === "About"){
-        activeLink.setAttribute("class","nav-link active");
-        console.log('Good!')
-    }
-    else if(activeLink.innerText === "My Work"){
-        activeLink.setAttribute("class","nav-link active");
-    }
-    console.log('Clicked!');
+    document.addEventListener('click', function(e){
+        activeLink = document.getElementById(e.target.id);
+        console.log(activeLink);
+        if (activeLink.innerText === "About"){
+            activeLink.setAttribute("class","nav-link active");
+            document.getElementById('my-work-link').setAttribute("class", "nav-link text-white");
+            document.getElementById('contact-link').setAttribute("class", "nav-link text-white");
+        }
+        else if(activeLink.innerText === "My Work"){
+            activeLink.setAttribute("class","nav-link active");
+            document.getElementById('about-link').setAttribute("class", "nav-link text-white");
+            document.getElementById('contact-link').setAttribute("class", "nav-link text-white");
+        }
+        else if (activeLink.innerText === "Alexander J. Cipollone"){
+            document.getElementById('about-link').setAttribute("class", "nav-link text-white");
+            document.getElementById('my-work-link').setAttribute("class", "nav-link text-white");
+            document.getElementById('contact-link').setAttribute("class", "nav-link text-white");
+        }
+        else {
+            activeLink.setAttribute("class","nav-link active");
+            document.getElementById('about-link').setAttribute("class", "nav-link text-white");
+            document.getElementById('my-work-link').setAttribute("class", "nav-link text-white");
+        }
+    })
 }
